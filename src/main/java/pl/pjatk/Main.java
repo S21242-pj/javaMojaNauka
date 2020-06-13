849c50065e243d3ef96f6b7b7a26436447e8f273
S21242-pj <56222962+S21242-pj@users.noreply.github.com>
2020-06-13 17:13:03 +0200

    Update Main.java

--- src/main/java/pl/pjatk/Main.java
+++ src/main/java/pl/pjatk/Main.java
@@ -6,11 +6,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+
         User user = new User("Szymon", "Dej", 1234);
-        Account account = new Account(BigDecimal.valueOf(1000), "00000000000000000000002212", user);
-        Bank bank = new Bank("BANk");
+        Account account = new Account(BigDecimal.valueOf(1000), "00123400000000000000002212", user);
+        Bank bank = new Bank("1234");
 
         bank.addAccountToBank(account);
+
 
         System.out.println(account);
         System.out.println(bank);
@@ -18,9 +20,21 @@ public class Main {
         Service service = new Service(bank);
         System.out.println(service.balanceAccount(user));
 
-        System.out.println(service.balanceAccount(new User("asdf", "asdf",1233)));
+        //System.out.println(service.balanceAccount(new User("asdf", "asdf",1233)));
+        System.out.println(service.checkBankAndAccountCorrelation(account));
+
+        service.topUpAccount(BigDecimal.valueOf(1000), user);
+        //System.out.println(service.balanceAccount(user));
+
+        User user1 = new User("asdf", "asdf", 1233);
+        Account account1 = new Account(BigDecimal.valueOf(2000), "00123422122212221222122212", user1);
+        bank.addAccountToBank(account1);
+
+
+        System.out.println(service.balanceAccount(user1));
+        System.out.println(service.balanceAccount(user));
+        service.transferMoney(user, user1, BigDecimal.valueOf(500));
+        System.out.println(service.balanceAccount(user1));
+        System.out.println(service.balanceAccount(user));
     }
-
-
-
 }
